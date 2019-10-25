6ddf1c9624729c3992dccc8a6155d76da70f9a80
anzairyo0127 <36684885+anzairyo0127@users.noreply.github.com>
2019-10-25 15:30:31 +0900

    fix test.ss

--- src/test.ts
+++ src/test.ts
@@ -7,25 +7,16 @@ const wait = (sec:number) => {
 };
 
 (async () => {
-    let mongo = await MongoDB.MongoClient.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true});
-    let testdb = mongo.db("testdb");
-    let col = testdb.collection("col");
+    const mongo = await MongoDB.MongoClient.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
+    const testdb = mongo.db("testdb");
+    const col = testdb.collection("col");
     await col.insertOne({ id: 1, text: "hogehoge"});
     for (let i: number = 0; i <= 100000; i++) {
         console.log(`${i}times`)
         console.log(`waiting....`)
         await wait(5);
-        try{
-            let id = await col.findOne({ id: 1 });
-            console.log(id);
-        } catch (e) {
-            console.log(e);
-            mongo.close();
-            mongo = await MongoDB.MongoClient.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true,});
-            testdb = mongo.db("testdb");
-            col = testdb.collection("col");
-            continue;
-        }
+        let id = await col.findOne({ id: 1 })
+        console.log(id);
     }
     mongo.close();
 })();
